7f43477e7701dfb3ea8baade3ec0cfaa232cdd44
Aaditya Arora <b17071@students.iitmandi.ac.in>
2018-11-06 16:51:00 +0530

    comments added

--- phase_1/hough_line.py
+++ phase_1/hough_line.py
@@ -22,6 +22,9 @@ lines = cv.HoughLinesP(dst, 1, math.pi/180.0, 40, np.array([]), 50, 10)
 a,b,c = lines.shape
 
 
+# actually it gives two points (x1 ,y1) and (x2,y2) so that A line can be drawn with this. Actually we need
+# points only ....so as to find two parallel lines and then finding mid line from this and then finding intersection.
+
 for i in range(a):
     cv.line(cdst, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255)   )
 
